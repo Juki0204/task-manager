2e0d173f504ed8d9ba8a625a82672b07b7f61964
Juki0204 <jukid0204@yahoo.co.jp>
2025-09-05 00:46:52 +0900

    未ログイン時にリダイレクトした際、getCurrentSession()がうまく実行されない不具合を修正

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -33,7 +33,7 @@ export default function AuthProvider({ children,
   useEffect(() => {
     getCurrentSession();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [pathname]);
 
   return (
     <>
