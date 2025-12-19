448731b8f012810ab8b7d946f1a6b0038f859045
Juki0204 <jukid0204@yahoo.co.jp>
2025-12-19 17:00:30 +0900

    幅1920px以上の時のcontextmenu表示位置調整

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -60,6 +60,7 @@ export default function PersonalTaskPage() {
 
   const handleContextMenu = (e: React.MouseEvent, taskId: string, taskSerial: string) => {
     setMenu({ visible: true, x: e.pageX, y: e.pageY, taskId, taskSerial });
+    console.log(e);
   }
 
   const handleCloseContextMenu = () => {
@@ -241,7 +242,7 @@ export default function PersonalTaskPage() {
   if (!isReady) return <p>loading...</p>
 
   return (
-    <div onClick={handleCloseContextMenu} className="cardListStyle p-1 py-4 sm:p-4 sm:pb-20 !pt-30 mx-auto max-w-[1920px] relative">
+    <div onClick={handleCloseContextMenu} className="cardListStyle p-1 py-4 sm:p-4 sm:pb-20 !pt-30 mx-auto max-w-[1920px]">
       <div className="flex justify-between gap-4 mb-2 border-b-2 p-1 pb-2 border-neutral-700 min-w-375">
         <div className="flex justify-start items-end gap-4">
           <h2 className="flex justify-center items-center gap-1 py-1 text-white text-xl font-bold text-center">
