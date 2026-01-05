40aeb57ccb86f43efdfdac423e40a8d2b0ef4b1a
Juki0204 <jukid0204@yahoo.co.jp>
2026-01-05 22:44:04 +0900

    完了タスクを年・月選択後に該当月分のみを取得する方式に仕様変更

--- src/app/complete/page.tsx
+++ src/app/complete/page.tsx
@@ -4,7 +4,7 @@
 import { useEffect, useMemo, useState } from "react";
 
 import { Task } from "@/utils/types/task";
-import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+import { Dialog, DialogBackdrop, DialogPanel, Select } from "@headlessui/react";
 
 import AddTask from "@/components/AddTask";
 import TaskList from "@/components/TaskList";
@@ -22,6 +22,10 @@ export default function CompletedTaskPage() {
   const [modalType, setModalType] = useState<"add" | "detail" | "edit" | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [activeTask, setActiveTask] = useState<Task | null>(null);
+
+  const [year, setYear] = useState<string>("");
+  const [month, setMonth] = useState<string>("");
+  const [isLoaded, setIsLoaded] = useState<boolean>(true);
 
   const [taskList, setTaskList] = useState<Task[]>([]);
   const { user } = useAuth();
@@ -65,14 +69,29 @@ export default function CompletedTaskPage() {
     }
   }
 
-  const getTasks = async () => {
-    const { data } = await supabase
+  const getTasks = async (year: string, month: string) => {
+    if (!year || !month) return;
+    setIsLoaded(false);
+    const m = month.padStart(2, "0");
+
+    const start = `${year}-${m}-01`;
+    const end = `${year}-${m}-31`;
+
+    const { data, error } = await supabase
       .from("tasks")
       .select("*")
-      .eq("status", "完了");
+      .eq("status", "完了")
+      .gte("finish_date", start)
+      .lte("finish_date", end);
+
+    if (error) {
+      console.error(error);
+      return false;
+    }
 
     if (!data) return false;
     setTaskList(data);
+    setIsLoaded(true);
   }
 
   const filteredTaskList = useMemo(() => {
@@ -104,16 +123,38 @@ export default function CompletedTaskPage() {
   }
 
   useEffect(() => {
-    getTasks();
-  }, []);
-
+    getTasks(year, month);
+  }, [year, month]);
 
   return (
     <div onClick={handleCloseContextMenu} className="p-1 py-4 sm:p-4 sm:pb-20 !pt-30 m-auto max-w-[1920px] relative">
       <div className="flex justify-between gap-4 mb-2 border-b-2 p-1 pb-2 border-neutral-700 min-w-375">
         <div className="flex justify-start items-end gap-4">
-          <h2 className="flex justify-center items-center gap-1 py-1 text-white text-xl font-bold text-center">
-            完了済タスク一覧
+          <h2 className="flex justify-center items-center gap-1 text-white text-xl font-bold text-center">
+            <span className="inline-block mr-2">完了済タスク一覧</span>
+            <Select onChange={(e) => setYear(e.target.value)} className="bg-neutral-700 rounded-md px-2 pt-0.5 pb-0.75">
+              <option value="">-</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+              <option value="2026">2026</option>
+            </Select>
+            年
+            <Select onChange={(e) => setMonth(e.target.value)} className="bg-neutral-700 rounded-md px-2 pt-0.5 pb-0.75">
+              <option value="">-</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </Select>
+            月度分
           </h2>
         </div>
 
@@ -122,7 +163,7 @@ export default function CompletedTaskPage() {
         </div> */}
       </div>
 
-      {user &&
+      {user && taskList.length > 0 ?
         <TaskList
           user={user}
           taskList={sortTask(filteredTaskList)}
@@ -141,7 +182,16 @@ export default function CompletedTaskPage() {
             setIsOpen(true);
           }}
           deadlineList={deadlineList}
-        />}
+        />
+        :
+        month && year && isLoaded ?
+          <div className="text-center text-white">該当するタスクがありません。</div>
+          :
+          !isLoaded ?
+            <div className="text-center text-white">取得中...</div>
+            :
+            <div className="text-center text-white">年月を選択すると該当月分の完了済みタスクが閲覧できます。</div>
+      }
 
       {/* 共通モーダル */}
       <Dialog
