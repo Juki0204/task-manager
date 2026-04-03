5eaa601c8b6a7a893927aabebf6f044443c89156
Juki0204 <jukid0204@yahoo.co.jp>
2026-04-03 11:10:18 +0900

    備考欄の未読 / 既読フラグの管理機能追加

--- src/components/PersonalCard.tsx
+++ src/components/PersonalCard.tsx
@@ -342,7 +342,7 @@ export default function PersonalCard({
           </span>
 
           {hasRemarksInfo && task.remarks && (
-            <RemarksHoverMark handleHover={setHover} className="absolute inset-y-0 right-0">
+            <RemarksHoverMark handleHover={setHover} task={task} className="absolute inset-y-0 right-0">
               {hover && remarksHtml && (
                 <div className={`whitespace-pre-wrap tiptap-base tiptap-viewer bg-neutral-100 py-1 px-2 rounded-md text-sm`} dangerouslySetInnerHTML={{ __html: remarksHtml }} />
               )}
